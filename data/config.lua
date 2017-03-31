d053cbd92b37b2c55bc41e62ba3d998dbe496c77
محمد <alm3lm.ms@gmail.com>
2017-03-31 08:59:32 -0700

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,30 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "fun",
+    "info",
+    "me",
+    "dell",
+    "cleanbot",
+    "rp"
+  },
+  info_text = "》Beyond Reborn v4.0\nAn advanced administration bot based on https://valtman.name/telegram-cli\n\n》https://github.com/BeyondTeam/BDReborn \n\n》Admins :\n》@SoLiD ➣ Founder & Developer《\n》@Makan ➣ Developer《\n》@Rixel ➣ Developer 《\n》@Exacute ➣ Developer《\n》@To0fan ➣ Developer《\n》@CiveY ➣ Developer\n》@ArmanDev ➣ Manager《\n》@MrPars ➣ Manager《\n\n》Special thanks to :\n》@Vysheng\n》@MrHalix\n》@K_a_I_i_I_i_n_u_x\n》@Nero_Dev\n》And Beyond Team Members\n\n》Our channel :\n》@BeyondTeam《\n\n》Our website :\n》http://BeyondTeam.ir\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    30742221,
+    367167027,
+    333768928,
+    117094188,
+    283167540
+  }
+}
+return _
+end
